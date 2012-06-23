5707a888ea4a58b1cfc61ad751a5a44754113809
Leon <leon@die-genossen.de>
2012-06-23 17:25:51 +0200

    added little print

--- src/de/hpi/cxftut/service/AdderServiceImpl.java
+++ src/de/hpi/cxftut/service/AdderServiceImpl.java
@@ -7,6 +7,7 @@ public class AdderServiceImpl implements AdderService {
 
 	@Override
 	public int add(int a, int b) {
+		System.out.printf("Oh my, I have to add %d and %d!", a, b);
 		return a+b;
 	}
 
