fb70e27b7cd35e60cc44e3ca84d68250d0d8b217
Leon <leon@die-genossen.de>
2012-06-23 17:32:50 +0200

    added async methods

--- src/de/hpi/cxftut/service/AdderServiceImpl.java
+++ src/de/hpi/cxftut/service/AdderServiceImpl.java
@@ -1,6 +1,12 @@
 package de.hpi.cxftut.service;
 
+import java.util.concurrent.Future;
+
 import javax.jws.WebService;
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
+
+import de.hpi.cxftut.service.jaxws.AddResponse;
 
 @WebService
 public class AdderServiceImpl implements AdderService {
@@ -11,4 +17,16 @@ public class AdderServiceImpl implements AdderService {
 		return a+b;
 	}
 
+	@Override
+	public Response<AddResponse> addAsync(int a, int b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Future<?> addAsync(int a, int b,
+			AsyncHandler<AddResponse> asyncHandler) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
