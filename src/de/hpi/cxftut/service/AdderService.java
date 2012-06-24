fef20f4f587746b10a4551df3775d60334a46080
Leon <leon@die-genossen.de>
2012-06-24 15:45:41 +0200

    remote client annotations

--- src/de/hpi/cxftut/service/AdderService.java
+++ src/de/hpi/cxftut/service/AdderService.java
@@ -1,12 +1,20 @@
 package de.hpi.cxftut.service;
 
 import javax.jws.*;		//contains the Annotations
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
 
 @WebService
 public interface AdderService {
 
     @WebMethod
+	@RequestWrapper(localName = "add",
+    		targetNamespace = "",
+            className = "de.hpi.cxftut.service.jaxws.Add")
+    @ResponseWrapper(localName = "addResponse",
+    		targetNamespace = "",
+            className = "de.hpi.cxftut.service.jaxws.AddResponse")
     public abstract int add(@WebParam(name="a")int a, @WebParam(name="b")int b);
 
 }
